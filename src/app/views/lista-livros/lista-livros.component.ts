bf18da4d09dcbf0f2dd3a48e98045dc86a250600
Karoline <kquetz@viceri.com.br>
2023-04-04 13:50:16 -0300

    implementada busca de livros

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent {
-
   listaLivros: [];
+  campoBusca: string = '';
+  constructor(private service: LivroService) {}
 
-  constructor() { }
-
+  buscarLivros() {
+    this.service.buscar(this.campoBusca).subscribe({
+      next: (retornoAPI) => console.log(retornoAPI),
+      error: (error) => console.error(error),
+      complete: () => console.log('Observable completado'),
+    });
+  }
 }
-
-
-
