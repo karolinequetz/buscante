31d0e5a4295820d32fd8def426379de5de5e0c14
Karoline <kquetz@viceri.com.br>
2023-04-05 14:46:26 -0300

    utilizado switchMap

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -13,9 +13,9 @@ export class LivroService {
   buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado);
     return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap((retornoAPI) => console.log(retornoAPI)),
-      map((resultado) => resultado.items),
-      tap((resultado) => console.log('Fluxo após o map', resultado))
+      //tap((retornoAPI) => console.log(retornoAPI)),
+      map((resultado) => resultado.items)
+      //tap((resultado) => console.log('Fluxo após o map', resultado))
     );
   }
 }
